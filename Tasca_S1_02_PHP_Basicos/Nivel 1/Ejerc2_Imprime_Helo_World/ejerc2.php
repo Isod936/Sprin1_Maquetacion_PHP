809b68c1725d2104477591420ee84253af7c2c57
Isod936 <skiba771@gmail.com>
2025-09-26 18:56:57 +0200

    Eliminar rastreo de submódulos para subir carpetas como normales

--- Tasca_S1_02_PHP_Basicos/Nivel 1/Ejerc2_Imprime_Helo_World/ejerc2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$saludo = "Hello World";
-echo strtoupper ($saludo ). "\n";
-echo strlen ($saludo). "\n";
-echo strrev ($saludo). "\n";
-$frase = "Este es el curso de PHP";
-echo "Saludo (". $saludo .") y frase (". $frase .") !\n";
-$comp = $saludo.", ". $frase. " !" ;
-echo $comp;
--- Tasca_S1_02_PHP_Basicos/Nivel 1/Ejerc3_Declara_Variable_Int/ejer3B.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo"Introduce un numero:";
-echo "Primer parametro: " . $argv[1];
-echo "Segundo parametro: ". $argv[2];
