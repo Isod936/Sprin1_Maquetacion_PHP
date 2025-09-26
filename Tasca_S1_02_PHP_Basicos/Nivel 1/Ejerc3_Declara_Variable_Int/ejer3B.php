b5b42339050af16dcc40d3f1de3d46ff306f2a91
Isod936 <skiba771@gmail.com>
2025-09-26 21:28:56 +0200

    Ejercicio_Calculadora_Modificado

--- Tasca_S1_02_PHP_Basicos/Nivel 1/Ejerc3_Declara_Variable_Int/ejer3B.php
+++ Tasca_S1_02_PHP_Basicos/Nivel 1/Ejerc3_Declara_Variable_Int/ejer3B.php
@@ -1,4 +1,39 @@
-<?php 
-echo"Introduce un numero:";
-echo "Primer parametro: " . $argv[1];
-echo "Segundo parametro: ". $argv[2];
+<?php
+echo "Introduce un numero:";
+$input1 = (int) readline("Introduce primer numero");
+if (!is_numeric($input1)) {
+    echo "El valor introducido no es un numero\n";
+    exit;
+}
+$num1 = (int) $input1;
+$input2 = (int) readline("Introduce primer numero");
+if (!is_numeric($input2)) {
+    echo "El valor introducido no es un numero\n";
+    exit;
+}
+$num2 = (int) $input2;
+$operador = readline("Introduce operacion (+,-,*,/): ");
+switch ($operador) {
+    case '+':
+        $resultado = $num1 + $num2;
+        "resultado :$resultado \n";
+        break;
+    case '-':
+        $resultado = $num1 - $num2;
+        "resultado :$resultado \n";
+        break;
+    case '*':
+        $resultado = $num1 * $num2;
+        "resultado :$resultado \n";
+        break;
+    case '/':
+        if ($num2 == 0) {
+            echo 'No se puede dividir por 0 \n';
+        } else {
+            $resultado = $num1 / $num2;
+            echo "resultado :$resultado \n";
+        }
+        break;
+    default:
+        echo 'El operador introducido no es correcto esta calculadora es de novatos';
+}
