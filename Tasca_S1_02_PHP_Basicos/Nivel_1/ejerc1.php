1fa9db9a3f519b8d63c5fc09f276c18f3dcf4773
Isod936 <skiba771@gmail.com>
2025-09-27 08:42:27 +0200

    cambio de nombres

---
+++ Tasca_S1_02_PHP_Basicos/Nivel_1/ejerc1.php
@@ -0,0 +1,15 @@
+<?php
+$modelo = 15; //String
+$peso = 171.5; // Double
+$nombre = "Iphone 15 pro"; // String
+$esDeApple = true; //Boolean
+$propietario = "Ivan"; //String
+
+echo "Nombre: $nombre\n";
+echo "Peso: $peso grammos\n";
+echo "Modelo: $modelo\n";
+echo "Es Appel ?: " . ($esDeApple ? "Sí" : "No") . "\n";
+echo "Nombre de propietario: $propietario\n";
+
+define("PROPIETARIO_TITULO", strtoupper($propietario));
+echo "-----" . PROPIETARIO_TITULO . "-----";
