3099bc878056c4bcb026f3e0f5a6f810e1ddf696
Isod936 <skiba771@gmail.com>
2025-10-08 08:57:44 +0200

    añadido

--- TascaS1_07_Errores_Y_Validaciones/Nivel1/DividirUnNumPor0.php
+++ TascaS1_07_Errores_Y_Validaciones/Nivel1/DividirUnNumPor0.php
@@ -1,6 +1,14 @@
 <?php
+function dividir($a, $b) {
+    try {
+        if ($b == 0) {
+            throw new Exception("No es pot dividir per zero.");
+        }
+        return $a / $b;
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
 
-
-
-
+echo dividir(10, 0);
 ?>
